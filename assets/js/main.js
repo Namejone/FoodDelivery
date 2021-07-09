0c541fdfcdb864471d664ea629e54988f426bff8
Nmaejone <devnamejone@gmail.com>
2021-07-09 16:59:09 +0600

    Serch food section for today(07/09/2021).Happy Codding

---
+++ assets/js/main.js
@@ -0,0 +1,48 @@
+(function ($) {
+    "use strict";
+    // owlCarousel
+    $('.popular-items-slider-active').owlCarousel({
+        loop:true,
+        margin:30,
+        items:5,
+        navText:['<i class="fal fa-angle-left"></i>','<i class="fal fa-angle-right"></i>'],
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            767:{
+                items:3
+            },
+            992:{
+                items:5
+            }
+        }
+    });
+
+    // owlCarousel
+    $('.search-products-active').owlCarousel({
+        loop:true,
+        margin:30,
+        items:5,
+        navText:['<i class="fal fa-angle-left"></i>','<i class="fal fa-angle-right"></i>'],
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            767:{
+                items:3
+            },
+            992:{
+                items:5
+            }
+        }
+    });
+
+
+
+    
+})(jQuery);
